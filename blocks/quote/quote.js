ea3c115a5e8b20be4d6d45aaf84815381de9d77b
Patricknshimiyimana <lennoxmupi@gmail.com>
2024-07-26 00:09:25 +0200

    create the neccessary blocks for creating news pages

---
+++ blocks/quote/quote.js
@@ -0,0 +1,13 @@
+export default function decorate(block) {
+  const quoteDiv = block.querySelector(':scope > div > div');
+  const blockquote = document.createElement('blockquote');
+  blockquote.innerHTML = `${quoteDiv.innerHTML}`;
+  quoteDiv.parentElement.replaceWith(blockquote);
+
+  const authorDiv = block.querySelector(':scope > div > div');
+  if (authorDiv) {
+    const p = document.createElement('p');
+    p.innerHTML = `<em> - ${authorDiv.innerText}</em>`;
+    authorDiv.parentElement.replaceWith(p);
+  }
+}
