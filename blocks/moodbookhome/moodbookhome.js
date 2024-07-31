8afe312bb5d630d2ee75d4217f155bdd5a507af6
Patricknshimiyimana <lennoxmupi@gmail.com>
2024-08-01 00:11:06 +0200

    improve moodbook blocks

--- blocks/moodbookhome/moodbookhome.js
+++ blocks/moodbookhome/moodbookhome.js
@@ -1,18 +1,14 @@
 export default function decorate(block) {
     
     const sectionDiv = block.querySelector(':scope > div').parentNode;
-    console.log("elementoo", sectionDiv.parentNode);
     
-    const content = `<div class="container">
-        <div class="left-column">
-            <h1>KOHRO</h1>
-            <h2>Italy</h2>
-            <h3>
-                NEW RELEASES<br>
-                2024
-            </h3>
+    const homeContent = block.querySelector(':scope > div > div').innerHTML;
+
+    const content = `<div class="home-container">
+        <div class="home-left-column">
+            ${homeContent}
         </div>
-        <div class="right-column">
+        <div class="home-right-column">
             <!-- This div is for the logo in the bottom right corner -->
         </div>
     </div>`
--- blocks/moodbooksection/moodbooksection.js
+++ blocks/moodbooksection/moodbooksection.js
@@ -1,10 +1,7 @@
 export default function decorate(block) {
     
     const sectionDiv = block.querySelector(':scope > div').parentNode;
-    console.log("main element", sectionDiv.parentNode);
-
     const pictureElement = block.querySelector(':scope > div > div > picture')
-    
     const containerDiv = sectionDiv.children[0];
 
     const rightColumnDiv = containerDiv.children[1].children[0];
@@ -12,14 +9,11 @@ export default function decorate(block) {
     imgEl.classList.add('inspiration-image')
 
     const leftColumnDiv = sectionDiv.children[1];
-    
     const subtitleText = leftColumnDiv.children[0].children[0]
     const titleText = leftColumnDiv.children[0].children[1];
-
     const description = leftColumnDiv.children[1];
 
     const colorsDiv = sectionDiv.children[2];
-    
     const colorImages = colorsDiv?.querySelector('div > p')
     
     const content = `<div class="container">
@@ -30,7 +24,7 @@ export default function decorate(block) {
                 <h1>${subtitleText.innerHTML}</h1>
             </div>
             <div class="right-column">
-                <div class="relaxed">${titleText.innerHTML} <div class="square"></div></div>
+                <div class="relaxed">${titleText.innerHTML} <div class="square" style="background: ${titleText.innerHTML.toLowerCase() == "glamour" ? "pink" : ""}"></div></div>
                 <picture>
                    ${rightColumnDiv.innerHTML}
                 </picture>
